7c758d39962fb778da3e93a77195da9620611004
Ali Hasanaj <42626041+alihasanaj@users.noreply.github.com>
2024-05-30 21:05:09 -0400

    testing

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ def main():
             if path[1] == "/":
                 response = b"HTTP/1.1 200 OK\r\n\r\n"
             if "echo" in path[1]:
-                string = path[1].strip("/echo/")
+                string = path[1].replace("/echo/", "")
                 response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(string)}\r\n\r\n{string}".encode()
             if "user" in path[1]:
                 path = args[2]
