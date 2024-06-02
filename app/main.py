28679d529686ff29aa54289e1a8e98524c8976e1
Ali Hasanaj <42626041+alihasanaj@users.noreply.github.com>
2024-06-02 17:10:09 -0400

    testing

--- app/main.py
+++ app/main.py
@@ -5,6 +5,14 @@ import os
 import sys
 import gzip
 
+# Global 
+# Default responses
+response_200 = b"HTTP/1.1 200 OK\r\n\r\n"
+response_404 = b"HTTP/1.1 404 Not Found\r\n\r\n"
+
+# Custom responses
+response_201 = f"HTTP/1.1 201 Created\r\n\r\n".encode()
+
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
@@ -16,24 +24,53 @@ def main():
         (conn, address) = server_socket.accept() # wait for client
         t =threading.Thread(target=lambda: request_handler(conn))
         t.start()
-    
+        
+
+def echo_request(request, encoding_check):
+    if request == "/":
+        return response_200
+    else:
+        string = request.replace("/echo/", "")
+        if "Encoding" in encoding_check:
+            string = gzip.compress(string.encode("utf-8"))
+            encoding_type = encoding_check.replace("Accept-Encoding: ", "")
+            if "," in encoding_type:
+                encoding_type = encoding_check.replace(" ", "")
+                encoding_type = encoding_type.split(",")
+            
+            
+            print(encoding_type)
+            if "gzip" == encoding_type:
+                return  f"HTTP/1.1 200 OK\r\nContent-Encoding: {encoding_type}\r\nContent-Type: text/plain\r\nContent-Length: {len(string)}\r\n\r\n".encode() + string
+            elif "gzip" in encoding_type:
+                index = encoding_type.index("gzip")
+                return f"HTTP/1.1 200 OK\r\nContent-Encoding: {encoding_type[index]}\r\nContent-Type: text/plain\r\nContent-Length: {len(string)}\r\n\r\n{string}".encode()
+            else:
+                return f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(string)}\r\n\r\n{string}".encode()
+        else:
+            return f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(string)}\r\n\r\n{string}".encode()
+        
+
+
+def post_request():
+    pass    
     
 def request_handler(conn: socket.socket):
-        val = conn.recv(1024)
-        pars = val.decode()
-        args = pars.split("\r\n")
-        print(f"Args: {args}")
+        client_data = conn.recv(1024)
+        decoded_data = client_data.decode()
+        arguments = decoded_data.split("\r\n")
+        print(f"arguments: {arguments}")
         # Split the request line and extract http info
-        request_line = args[0].split(" ")
+        request_line = arguments[0].split(" ")
         http_method = request_line[0]
         request_target = request_line[1]
         http_version = request_line[2]
         
         # Split the headers line
-        header_host = args[1]
-        user_agent = args[2]
-        media_type = args[-2]
-        req_body = args[-1]
+        header_host = arguments[1]
+        user_agent = arguments[2]
+        media_type = arguments[-2]
+        req_body = arguments[-1]
         
         print(f"http method: {http_method}")
         print(f"request target: {request_target}")
@@ -45,45 +82,16 @@ def request_handler(conn: socket.socket):
         print(f"Body: {req_body}")
             
             
-        # Default responses
-        response_200 = b"HTTP/1.1 200 OK\r\n\r\n"
-        response_404 = b"HTTP/1.1 404 Not Found\r\n\r\n"
         
-        # Custom responses
-        response_201 = f"HTTP/1.1 201 Created\r\n\r\n".encode()
+        
         
         # supported encoding
         supported_encodes = ["gzip"]
         
         response = response_404
-        if len(args) > 1:
-            if request_target == "/":
-                response = response_200
-            elif request_target.startswith("/echo/"):
-                string = request_target.replace("/echo/", "")
-                
-                if "Encoding" in user_agent:
-                    string = gzip.compress(string.encode("utf-8"))
-                    encoding_type = user_agent.replace("Accept-Encoding: ", "")
-                    if "," in encoding_type:
-                        encoding_type = user_agent.replace(" ", "")
-                        encoding_type = encoding_type.split(",")
-                    
-                    
-                    print(encoding_type)
-                    if "gzip" == encoding_type:
-                        response = f"HTTP/1.1 200 OK\r\nContent-Encoding: {encoding_type}\r\nContent-Type: text/plain\r\nContent-Length: {len(string)}\r\n\r\n".encode() + string
-                    elif "gzip" in encoding_type:
-                        index = encoding_type.index("gzip")
-                        response = f"HTTP/1.1 200 OK\r\nContent-Encoding: {encoding_type[index]}\r\nContent-Type: text/plain\r\nContent-Length: {len(string)}\r\n\r\n{string}".encode()
-                    else:
-                        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(string)}\r\n\r\n{string}".encode()
-                else:
-                    response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(string)}\r\n\r\n{string}".encode()
-            elif "user" in request_target:
-                string = user_agent.replace("User-Agent: ", "")
-                response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(string)}\r\n\r\n{string}".encode()
-            elif "files" in request_target:
+        if "echo" in request_target:
+            response = echo_request(request=request_target, encoding_check=user_agent) 
+        elif "files" in request_target:
                 if "POST" in http_method:
                     directory = sys.argv[2]
                     try:
